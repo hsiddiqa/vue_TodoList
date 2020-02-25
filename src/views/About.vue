7eaa93bda6d8e4afe9402bf66db5f570f4e3a0be
Humaira Siddiqa <humaira.siddiqa@gmail.com>
2020-02-25 11:16:37 -0500

    Adding the project to git

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the Todo List application. This application has been created following 
+      a youtube tutorial.
+    </p>
+  </div>
+</template>
